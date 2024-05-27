2010e78bd4a1f370c2ef8fc73006caaac0a4a672
JWSch4fer <JWSch4fer@github.com>
2024-05-27 17:55:04 -0400

    gremlin v1

--- GREMLIN.py
+++ GREMLIN.py
@@ -4,9 +4,11 @@ import sys
 
 from scipy.spatial.distance import pdist, squareform
 import numpy as np
+import optax
 import jax
 import jax.numpy as jnp
 from jax.numpy import array
+from jax.scipy.special import logsumexp
 
 class PREPROCESS_MSA():
     """
@@ -130,119 +132,70 @@ def GREMLIN(seqs: array, iterations=100, gaps=True):
     # Initialize parameters
     states = 21 #20 aa + gap
     #use a bitmap to encode the integer version of the msa
-    msa_one_hot = jax.nn.one_hot(seqs_int, states)
+    msa_bitmap = jax.nn.one_hot(seqs_int, states)
+    NO_GAP = False
     if not gaps:
         states = states - 1
-        NO_GAP = 1.0 - msa_one_hot[...,-1] 
-        msa_one_hot = msa_one_hot[...,:states]
+        NO_GAP = 1.0 - msa_bitmap[...,-1] 
+        msa_bitmap = msa_bitmap[...,:states]
 
-    V = jnp.tensordot(msa_one_hot, seqs_weights, axes=[0,0])
+    #prep one-body term
+    one_body = jnp.tensordot(msa_bitmap, seqs_weights, axes=[0,0])
     pseudo_count = 0.01 * jnp.log(jnp.sum(seqs_weights))
-    V = jnp.log(jnp.add(V , pseudo_count))
-    V = jnp.add(V,-1*jnp.mean(V, axis=1, keepdims=True))
-    V_init = V
+    one_body = jnp.log(jnp.add(one_body , pseudo_count))
+    one_body = jnp.add(one_body,-1*jnp.mean(one_body, axis=1, keepdims=True))
+    one_body_init = one_body
+    
+    #prep two-body term
+    two_body_init = jnp.zeros((one_body.shape[0], states, one_body.shape[0], states))
 
-    V_dims = V_init.shape; V_idx = jnp.prod(jnp.array(V_dims))
-    W_init = jnp.zeros((ncol, states, ncol, states))
-    W_dims = W_init.shape; W_idx = jnp.prod(jnp.array(W_dims))
-    print(V_idx, W_idx)
-    print(V_dims, W_dims)
-    print(V_init.shape)
-    print(W_init.shape)
-    print(msa_one_hot.shape)
-    print(weights.shape)
-    print(sum(weights))
-    print(msa['msa'])
-    # Optimizer setup
-    # optimizer = optax.adam(learning_rate=opt_rate)
-    # opt_state = optimizer.init((V_init, W_init))
-    # 
-    # @jax.jit
-    # def loss_fn(params, msa_one_hot, weights):
-    #     V, W = params
-    #     VW = V + jnp.tensordot(msa_one_hot, W, axes=2)
-    #     H = jnp.sum(msa_one_hot * VW, axis=-1)
-    #     Z = logsumexp(VW, axis=-1)
-    #     PLL = H - Z
-    #     PLL = jnp.sum(PLL, axis=-1)
-    #     PLL_weighted = jnp.sum(weights * PLL) / jnp.sum(weights)
-    #
-    #     # Regularization
-    #     L2_V = lam_v * jnp.sum(jnp.square(V))
-    #     L2_W = lam_w * jnp.sum(jnp.square(W)) * 0.5
-    #     if scale_lam_w:
-    #         L2_W *= (ncol - 1) * (states - 1)
-    #
-    #     return -PLL_weighted + (L2_V + L2_W) / msa["neff"]
-    # @jax.jit
-    # def step(params, msa_one_hot, weights, opt_state):
-    #     grads = jax.grad(loss_fn)(params, msa_one_hot, weights)
-    #     updates, opt_state = optimizer.update(grads, opt_state)
-    #     params = optax.apply_updates(params, updates)
-    #     return params, opt_state
-
-    # Initial parameters
-    # params = (V_init, W_init)
-    # @jax.jit
-    def loss_fn(x, V_dims=V_dims, V_idx=V_idx, W_dims=W_dims, W_idx=W_idx):
-        V = x[0:V_idx].reshape(V_dims)
-        W_tmp = x[V_idx:].reshape(W_dims)
-
+    @jax.jit
+    def loss(body, one_body_dims = one_body_init.shape, two_body_dims = two_body_init.shape):
+        _ = one_body_dims[0] * one_body_dims[1]
+        one_body = body[0:_].reshape(one_body_dims)
+        two_body = body[_:].reshape(two_body_dims)
         # Symmetrize W
-        W = W_tmp + jnp.transpose(W_tmp, (2, 3, 0, 1))
+        two_body = two_body + jnp.transpose(two_body, (2, 3, 0, 1))
 
         # Set diagonal to zero
-        W = W * (1 - jnp.eye(ncol))[:, None, :, None]
+        two_body = two_body * (1 - jnp.eye(one_body.shape[0]))[:, None, :, None]
 
-        VW = V + jnp.tensordot(msa_one_hot, W, axes=2)
-        H = jnp.sum(msa_one_hot * VW, axis=-1)
-        Z = logsumexp(VW, axis=-1)
-        PLL = H - Z
+        one_two = one_body + jnp.tensordot(msa_bitmap, two_body, axes=2)
+        markov_blanket = jnp.sum(msa_bitmap * one_two, axis=-1)
+        partition = logsumexp(one_two, axis=-1)
+        PLL = markov_blanket - partition
 
-        PLL = PLL * NO_GAP
+        def if_true(PLL: array):
+            return PLL 
+        def if_false(PLL: array):
+            return PLL * NO_GAP
+        PLL = jax.lax.cond(gaps == True,if_true, if_false , PLL)
         PLL = jnp.sum(PLL, axis=-1)
-        PLL_weighted = jnp.sum(weights * PLL) / jnp.sum(weights)
+        PLL_weighted = jnp.sum(seqs_weights * PLL) / jnp.sum(seqs_weights)
 
-        # Regularization
-        L2_V =  0.01 * jnp.sum(jnp.square(V))
-        L2_W =  0.1 * jnp.sum(jnp.square(W)) * 0.5
-        L2_W =  L2_W * (ncol - 1) * (states - 1)
-        return -PLL_weighted + (L2_V + L2_W) / jnp.sum(weights)
-    # t_i = time.time()
-    # res = minimize(loss_fn, W_init.ravel(), args=(V_init, W_init.shape),  method='BFGS', tol=1e-3)
-    # t_f = time.time()
-    #
-    # print(res.x, res.fun, res.success, res.nfev, res.njev)
-    # print('JAX calculation took {:0.6f} secs.'.format(t_f-t_i))
+        # Regularization to prevent overfitting
+        L2_V =  0.01 * jnp.sum(jnp.square(one_body))
+        L2_W =  0.1 * jnp.sum(jnp.square(two_body)) * 0.5
+        L2_W =  L2_W * (one_body.shape[0] - 1) * (states - 1)
+        return -PLL_weighted + (L2_V + L2_W) / jnp.sum(seqs_weights)
+
+    @jax.jit
+    def opt_step(iterate, _):
+        x, opt_state = iterate
+        grad = jax.grad(loss)(x)
+        updates, opt_state = solver.update(grad, opt_state, x)
+        x = optax.apply_updates(x, updates)
+        return (x, opt_state), loss(x)
 
     solver = optax.adadelta(learning_rate=1.)
-    # params = W_init
-    x0 = jnp.concatenate([V_init.ravel(), W_init.ravel()])
+    x_0 = jnp.concatenate([one_body_init.ravel(), two_body_init.ravel()])
+    opt_state = solver.init(x_0)
+    x_final, losses = jax.lax.scan(opt_step, (x_0, opt_state), None, length=iterations)
+    print(losses)
+    one_body_final = x_final[0].reshape(one_body_init.shape)
+    two_body_final = x_final[1].reshape(two_body_init.shape)
 
-    opt_state = solver.init(x0)
-    print(loss_fn(x0) * jnp.sum(weights))
-    for _ in range(100):
-        grad = jax.grad(loss_fn)(x0)
-        updates, opt_state = solver.update(grad, opt_state, x0)
-        x0 = optax.apply_updates(x0, updates)
-        print('Objective function: {:.2E}'.format(loss_fn(x0)* jnp.sum(weights)))
-    # W_final = x0
-    V_final = x0[0:V_idx].reshape(V_dims)
-    # V_final = V_init
-    W_final = x0[V_idx:].reshape(W_dims)
-
-    # Training loop
-    # temp = []
-    # for i in range(opt_iter):
-    #     params, opt_state = step(params, msa_one_hot, weights, opt_state)
-    #     temp.append(loss_fn(params, msa_one_hot, weights))
-    #     if (i + 1) % 10 == 0:
-    #         print(f"Iteration {i+1} loss = {loss_fn(params, msa_one_hot, weights)}")
-    #
-    # V_final, W_final = params
-    return {"v": V_final, "w": W_final, "v_idx": msa["v_idx"], "len": msa["ncol_ori"]}
-    return np.array(temp)
-
+    return one_body_final, two_body_final
 
 
 def main(fasta, filter_msa, filter_msa_row, filter_msa_col):
@@ -262,7 +215,8 @@ def main(fasta, filter_msa, filter_msa_row, filter_msa_col):
         names = preprocess_msa.names
         aa = preprocess_msa.aa
 
-    GREMLIN(seqs)
+    one_body, two_body = GREMLIN(seqs)
+    print(one_body)
    
 if  __name__ == '__main__':
     main(*CL_input())
